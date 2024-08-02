a161fff31451a0406f11d5c7228d96bedd6c5d1b
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-02 18:46:56 -0400

    add the listeners internally

--- index.js
+++ index.js
@@ -3,20 +3,27 @@ import EventEmitter from "node:events";
 class PizzaShop extends EventEmitter {
   constructor() {
     super();
+    this.initializeListeners();
   }
 
   createOrder(pizzaType) {
     console.log(`making you a ${pizzaType}`);
     setTimeout(() => {
-      console.log("Your pizza is ready!");
+      this.emit("order-ready");
     }, 1000);
+  }
+
+  initializeListeners() {
+    this.on("order-ready", () => {
+      console.log("Your pizza is ready!");
+    });
+
+    this.on("order", () => {
+      this.createOrder("hawaiian");
+    });
   }
 }
 
 const lukesPizza = new PizzaShop();
-
-lukesPizza.on("order", () => {
-  lukesPizza.createOrder("hawaiian");
-});
 
 lukesPizza.emit("order");
