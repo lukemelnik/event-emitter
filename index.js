7557d5a55d248834b0f6c578b66ae1a321e6c7ce
Luke Melnik <70446784+lukemelnik@users.noreply.github.com>
2024-08-02 18:52:01 -0400

    add a second instance

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import EventEmitter from "node:events";
 
 class PizzaShop extends EventEmitter {
-  constructor() {
+  constructor(company) {
     super();
     this.initializeListeners();
   }
 
-  createOrder(pizzaType) {
-    console.log(`making you a ${pizzaType}`);
+  createOrder(type) {
+    console.log(`making you a ${type} pizza`);
     setTimeout(() => {
       this.emit("order-ready");
     }, 1000);
@@ -18,12 +18,16 @@ class PizzaShop extends EventEmitter {
       console.log("Your pizza is ready!");
     });
 
-    this.on("order", () => {
-      this.createOrder("hawaiian");
+    this.on("order", (type) => {
+      this.createOrder(type);
     });
   }
 }
 
-const lukesPizza = new PizzaShop();
+const lukesPizza = new PizzaShop("Luke's Pizza");
 
-lukesPizza.emit("order");
+lukesPizza.emit("order", "hawaiian");
+
+const finnsPizza = new PizzaShop("Finn's Pizza");
+
+finnsPizza.emit("order", "canadian");
